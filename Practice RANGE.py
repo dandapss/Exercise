c6afb85df0b87221b86cf2dc0c50df1c253ec4de
dandapss <dandap23@gmail.com>
2024-07-25 23:16:26 +0900

    Practice RANGE.py

--- Practice RANGE.py
+++ Practice RANGE.py
@@ -1,7 +1,5 @@
 # sum of even numbers between 0 and certain number
-
-number = int(input())
-
+number = int(input("What is your number?: "))
 t1=0
 for t in range(2, number+1):
     if t%2 >= 1:
@@ -21,7 +19,7 @@ print("Rule No.1 : If the number is divisible by 3, call 'FIZZ' instead of the n
 print("Rule No.2 : If the number is divisible by 5, call 'BUZZ' instead of the number.")
 print("Rule No.3 : If the nubmer is divisible by both 3 and 5 call 'FIZZBUZZ'.")
 
-numbers = int(input())
+numbers = int(input("What are your numbers?: "))
 fizz = 0
 for number in range(1, numbers+1):
     if number%3 == 0 and number%5 == 0:
